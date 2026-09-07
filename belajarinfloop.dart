cc64ad89dbac931f775d335ef5044fa0935bda32
LouisVantaoo-oss <louisfernando2006@gmail.com>
2026-09-07 20:52:45 +0700

    first commit

--- belajarinfloop.dart
+++ belajarinfloop.dart
@@ -1,3 +1,58 @@
-void main () {
-    
+void main() {
+ //  int kehadiran = 75;
+
+ //  if (kehadiran < 75) {
+ //    print('tidak bisa ikut ujian');
+ //  } else {
+ //    print('bisa ikut ujian');
+ //  }
+
+ //   if (kehadiran < 75) {
+ //    print('tidak bisa ikut ujian');
+ //  }
+  
+ //  if (kehadiran >= 75) {
+ //    print('bisa ikut ujian');
+ //  }
+
+ // var nilaiAkhir = 'A';
+ // switch (nilaiAkhir) {
+ //   case 'A':
+ //   print('Nilai Sangat Memuaskan');
+ //   break;
+ //   case 'B':
+ //   print('Nilai Memuaskan');
+ //   break;
+ //   default:
+ //   print('Nilai Tidak Memuaskan');
+ // }
+
+
+ //perulangan
+ // List datamhs = ['andri', 'budi', 'caca', 'didi'];
+ // for (var i = 0; i < datamhs.length; i++) {
+ //     //nilai awal, batas, increment
+ //   print(datamhs[i]);
+ // }
+
+  // for (var element in datamhs) {
+  //   print(element);
+  // }
+  
+  // datamhs.forEach(print);
+
+  // var index = 0;
+  // while (index < 5) {
+  //   print('datamhs[index]');
+  //   index++;
+  // }
+
+  // var datamhs = ['andri', 'budi', 'caca', 'didi'];
+  // var index = 0;
+  // do {
+  //   print(datamhs[index]);
+  //   index++;
+  // } while (index < datamhs.length);
+
+
 }
